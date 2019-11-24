f2c4610e4a4fb8e68226a93ce6e885f0a8f9d3b7
doanhnn <35711618+doanhnn@users.noreply.github.com>
2019-11-24 07:25:18 -0500

    add exception

--- Bai 3/ex3.py
+++ Bai 3/ex3.py
@@ -1,42 +1,44 @@
 from PIL import Image
 import numpy as np
 
-img = Image.open('img.png')
-print "Image mode:"+ img.mode
+img = Image.open('pic.rgba')
+print "Image Mode: " + img.mode
 
 rgba2grey = img.convert('LA')
 rgba2grey.save('greyscale.png')
 
+try:
+	data = img.getdata()
+	r = [(d[0], 0, 0) for d in data]
+	g = [(0, d[1], 0) for d in data]
+	b = [(0, 0, d[2]) for d in data]
+	img_r = img.copy()
+	img_r.putdata(r)
+	img_r.save('redscale.png')
 
-data = img.getdata()
-r = [(d[0], 0, 0) for d in data]
-g = [(0, d[1], 0) for d in data]
-b = [(0, 0, d[2]) for d in data]
-img_r = img.copy()
-img_r.putdata(r)
-img_r.save('redscale.png')
+	img_g = img.copy()
+	img_g.putdata(g)
+	img_g.save('greenscale.png')
 
-img_g = img.copy()
-img_g.putdata(g)
-img_g.save('greenscale.png')
+	img_b = img.copy()
+	img_b.putdata(b)
+	img_b.save('bluescale.png')
 
-img_b = img.copy()
-img_b.putdata(b)
-img_b.save('bluescale.png')
+	if img.mode == 'RGBA':
+		img_a = img.copy()
+		img_a.putalpha(128)
+		img_a.save('alphascale.png')
 
-if img.mode == 'RGBA':
-	img_a = img.copy()
-	img_a.putalpha(128)
-	img_a.save('alphascale.png')
+	R = Image.open('redscale.png').split()[0]
+	G = Image.open('greenscale.png').split()[1]
+	B = Image.open('bluescale.png').split()[2]
 
-R = Image.open('redscale.png').split()[0]
-G = Image.open('greenscale.png').split()[1]
-B = Image.open('bluescale.png').split()[2]
-
-if img.mode == 'RGB':
-	RGB = Image.merge("RGB",(R,G,B))
-	RGB.save('merged.png')
-else:
-	A = Image.open('alphascale.png').split()[3]
-	RGBA = Image.merge("RGBA",(R,G,B,A))
-	RGBA.save('merged.png')
+	if img.mode == 'RGB':
+		RGB = Image.merge("RGB",(R,G,B))
+		RGB.save('merged.png')
+	else:
+		A = Image.open('alphascale.png').split()[3]
+		RGBA = Image.merge("RGBA",(R,G,B,A))
+		RGBA.save('merged.png')
+except:
+	print("Input file again. Only accept RGB, RGBA !")
