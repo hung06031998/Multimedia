34518535029dcd6f5fcfe43f2cf991e38a3b479c
HoanGnR <47739011+HoanGnR@users.noreply.github.com>
2019-11-24 22:47:46 +0700

    Fix generator's bug

--- Bai 2/bai2.py
+++ Bai 2/bai2.py
@@ -31,13 +31,16 @@ def create_and_save(file_name, freq):
     return
 #main
 freq_list = [440]
+k = 1.05946309436 # 2 ** 1/12
 for t in range(1,12) :
-	freq_list.append(freq_list[t-1] * (2 ** 1/12))
+
+	freq_list.append(freq_list[t-1] * k)
         note_name =       ["A4.wav","A#4.wav","B4.wav","C5.wav","C#5.wav","D5.wav", "D#5.wav", "E5.wav", "F5.wav", "F#5.wav","G5.wav","G#5.wav"] 	   		
         freq_note = [[x, y] for x, y in zip (note_name, freq_list)]
 
 
 for [filename, freq] in freq_note :
 	 print("Creating" + filename)
+	 print freq
 	 create_and_save(filename, freq)
 print ("Done")
